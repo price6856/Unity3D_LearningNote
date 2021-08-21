4c9039c8d44fc539a909be41f344c39b8aac72aa
price6856 <1075915477@qq.com>
2021-08-21 21:52:44 +0800

    Eighth Commit

--- MyUnityProject/Assets/Scripts/CarCameraFollow.cs
+++ MyUnityProject/Assets/Scripts/CarCameraFollow.cs
@@ -1,23 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class CarCameraFollow : MonoBehaviour
 {
+    
     private Transform target;
     private Vector3 fixedDistance;
     private Vector3 tempPos;
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;//找到标签目标物体
-        fixedDistance = new Vector3(-2.08f, 1.58f, -2.97f);//相机处于赛车背后的初始位置，需要进行测试
+       
+        fixedDistance = new Vector3(-2.0f, 1.5f, -2.5f);//相机处于赛车背后的初始位置，需要进行测试
     }
     void FixedUpdate()
-    {
+    { 
+       
         //transformDirection（new Vector3）可以将Vector3的坐标转换为世界坐标；
         tempPos = target.TransformDirection(fixedDistance) + target.position;
         transform.position = Vector3.Lerp(transform.position, tempPos, Time.fixedDeltaTime * 3);
-    transform.LookAt(target);//使相机自身z坐标对齐物体的自身z坐标（注意不是世界坐标）
+        transform.LookAt(target);//使相机自身z坐标对齐物体的自身z坐标（注意不是世界坐标）
+    }
+    
+    
+
+    /*
+    private Transform player;
+    private Vector3 repos;
+    private Vector3 direction;
+    void Awake()
+    {
+        //获取到player的transform(Tags.Player是player的标签，如果有不明白什么意思的，可以看我前几篇文章，标签的管理)
+        player = GameObject.FindWithTag("Player").transform;
+        
+        //计算player到camera的方向向量的距离
+        direction = player.position - transform.position;
+        repos = GetRelativePosition(player, transform.position);
+    }
+    void Update()
+    {
+        //移动摄像机，使摄像机与player保持一定的方向向量
+        //camera当前的位置=player的位置减去方向向量
+        //transform.position = player.position - direction;
+        transform.position = player.position+repos;
+        //transform.position = player.position - direction;
+        //transform.LookAt(player);
     }
 
+    private Vector3 GetRelativePosition(Transform origin, Vector3 position)
+    {
+        Vector3 distance = position - origin.position;
+        Vector3 relativePosition = Vector3.zero;
+        relativePosition.x = Vector3.Dot(distance, origin.right.normalized);
+        relativePosition.y = Vector3.Dot(distance, origin.up.normalized);
+        relativePosition.z = Vector3.Dot(distance, origin.forward.normalized);
+        return relativePosition;
+    }
+    */
 }
--- MyUnityProject/Assets/Scripts/CarController.cs
+++ MyUnityProject/Assets/Scripts/CarController.cs
@@ -9,6 +9,144 @@ public class CarController : MonoBehaviour
     public List<AxleInfo> axleInfos;
     public float maxMotorTorque;
     public float maxSteeringAngle;
+    public GameObject frontleftw, frontrightw, rearleftw, rearrightw,car;
+    public float time;
+    public float time1;
+    public Vector3 lastpos,carpos;
+    void Start()
+    {
+        // frontleftw = GameObject.FindWithTag("frontwheel");
+        frontleftw = GameObject.Find("frontLeft");
+        frontrightw = GameObject.Find("frontRight");
+        rearleftw = GameObject.Find("rearLeft");
+        rearrightw = GameObject.Find("rearRight");
+        car = GameObject.Find("Car8");
+
+
+    }
+    void Update()
+    {
+        //车轮旋转
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            if (frontleftw.GetComponent<Transform>().localEulerAngles.y == 0 && frontrightw.GetComponent<Transform>().localEulerAngles.y == 0) {
+                //frontleftw.transform.Rotate(0, -30, 0);
+                //frontrightw.transform.Rotate(0, -30, 0);
+                frontleftw.transform.localEulerAngles = new Vector3(0, -30, 0);
+                frontrightw.transform.localEulerAngles = new Vector3(0, -30, 0);
+                Debug.Log("向左旋转30°");
+            }
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            if (frontleftw.GetComponent<Transform>().localRotation.y == 0 && frontrightw.GetComponent<Transform>().localRotation.y == 0)
+            {
+                //frontleftw.transform.Rotate(0, 30, 0);
+                //frontrightw.transform.Rotate(0, 30, 0);
+                frontleftw.transform.localEulerAngles = new Vector3(0, 30, 0);
+                frontrightw.transform.localEulerAngles = new Vector3(0, 30, 0);
+                Debug.Log("向右旋转30°");
+            } 
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            frontleftw.transform.localEulerAngles = new Vector3(0, 0, 0);
+            frontrightw.transform.localEulerAngles = new Vector3(0, 0, 0);
+            Debug.Log("回正");
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            frontleftw.transform.localEulerAngles = new Vector3(0, 0, 0);
+            frontrightw.transform.localEulerAngles = new Vector3(0, 0, 0);
+            Debug.Log("回正");
+        }
+        
+        //车轮滚动
+        if (Input.GetKey(KeyCode.W))
+        {
+            frontleftw.transform.Rotate(5,0,0);
+            frontrightw.transform.Rotate(5,0,0);
+            rearleftw.transform.Rotate(5, 0, 0);
+            rearrightw.transform.Rotate(5, 0, 0);
+            Debug.Log("向前滚动");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            frontleftw.transform.Rotate(-5, 0, 0);
+            frontrightw.transform.Rotate(-5, 0, 0);
+            rearleftw.transform.Rotate(-5, 0, 0);
+            rearrightw.transform.Rotate(-5, 0, 0);
+            Debug.Log("向后滚动");
+        }
+        
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            time1 = Time.time;
+            lastpos = frontleftw.transform.position;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Rigidbody rigidbody = car.transform.GetComponent<Rigidbody>();
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+            rigidbody.constraints = RigidbodyConstraints.None;
+        }
+        
+        //lastpos = frontleftw.transform.position;
+
+        if (Time.time - time1 > 0.1)
+        {
+            time1 = Time.time;
+            //判断是否有移动
+            if ((frontleftw.transform.position - lastpos).sqrMagnitude > 0.01f && (frontleftw.transform.position - lastpos).z > 0)
+            {
+                frontleftw.transform.Rotate(240, 0, 0);
+                frontrightw.transform.Rotate(240, 0, 0);
+                rearleftw.transform.Rotate(240, 0, 0);
+                rearrightw.transform.Rotate(240, 0, 0);
+            } else if ((frontleftw.transform.position - lastpos).sqrMagnitude > 0.01f && (frontleftw.transform.position - lastpos).z < 0)
+            {
+                frontleftw.transform.Rotate(-240, 0, 0);
+                frontrightw.transform.Rotate(-240, 0, 0);
+                rearleftw.transform.Rotate(-240, 0, 0);
+                rearrightw.transform.Rotate(-240, 0, 0);
+            } 
+            else if ((frontleftw.transform.position - lastpos).sqrMagnitude > 0.001f && (frontleftw.transform.position - lastpos).z > 0)
+            {
+                frontleftw.transform.Rotate(80, 0, 0);
+                frontrightw.transform.Rotate(80, 0, 0);
+                rearleftw.transform.Rotate(80, 0, 0);
+                rearrightw.transform.Rotate(80, 0, 0);
+
+            }
+            else if ((frontleftw.transform.position - lastpos).sqrMagnitude > 0.001f && (frontleftw.transform.position - lastpos).z < 0)
+            {
+                frontleftw.transform.Rotate(-80, 0, 0);
+                frontrightw.transform.Rotate(-80, 0, 0);
+                rearleftw.transform.Rotate(-80, 0, 0);
+                rearrightw.transform.Rotate(-80, 0, 0);
+            }
+            else
+            {
+                frontleftw.transform.localEulerAngles = new Vector3(0, 0, 0);
+                frontrightw.transform.localEulerAngles = new Vector3(0, 0, 0);
+                rearleftw.transform.localEulerAngles = new Vector3(0, 0, 0);
+                rearrightw.transform.localEulerAngles = new Vector3(0, 0, 0);
+            }
+            lastpos = frontleftw.transform.position;
+        }
+
+
+
+    }
 
     // finds the corresponding visual wheel
     // correctly applies the transform
